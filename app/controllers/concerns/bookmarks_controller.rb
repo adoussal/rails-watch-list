26385dbee0d54c972707ef95c1c5c14b956682ce
Adrien Le Doussal <adoussal@gmail.com>
2023-06-16 12:48:56 +0200

    fixing bundle lock

--- app/controllers/concerns/bookmarks_controller.rb
+++ app/controllers/concerns/bookmarks_controller.rb
@@ -1,15 +1,36 @@
 class BookmarksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
-    @bookmark = @list.bookmarks.new
+    @bookmark = Bookmark.new
   end
 
   def create
     @list = List.find(params[:list_id])
-    @bookmark = @list.bookmarks.new(bookmark_params)
-    @bookmark.save!
-    redirect_to list_path(@list)
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save(bookmark_params)
+      redirect_to list_path(@list)
+      flash.now[:notice] = 'Bookmark was successfully created.'
+    else
+      flash.now[:alert] = @bookmark.errors.full_messages.join(', ')
+      render :new
+    end
   end
+
+  # def edit
+  #   @list = List.find(params[:list_id])
+  #   @bookmark = Bookmark.find(params[:id])
+  # end
+
+  # def update
+  #   @list = List.find(params[:list_id])
+  #   @bookmark = Bookmark.find(params[:id])
+  #   if @bookmark.update(bookmark_params)
+  #     redirect_to list_path(@list), notice: 'Bookmark was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-
   validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list }
+  validates :movie, uniqueness: { scope: :list, message: 'is already in the list' }
 end
